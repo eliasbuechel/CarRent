819ab4f7ec35f95ee604400fedbc533aa9a124e8
Elias Büchel <eli.buechel@hotmail.com>
2023-07-03 11:36:32 +0200

    customer management initial slice

--- source/src/CarRent/Program.cs
+++ source/src/CarRent/Program.cs
@@ -1,6 +1,10 @@
+using CarRent.CustomerManagement.Infrastructure.Persitence;
+using CarRent.CustomerManagement.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,14 +16,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
